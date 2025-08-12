5763ac85aaf1992b78cf13afda000050b0a9ca54
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-08-12 07:33:15 +0100

    Name

---
+++ Counting.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Counting{
+	public static void main(String [] args){
+		for(int count = 1; count <= 5;count++){
+			for(int counter = 0; counter <= 5; counter++){
+			System.out.print("Number" + counter + " ");
+ 					}
+			System.out.println();
+			
+			}
+		}
+
+
+
+	}
